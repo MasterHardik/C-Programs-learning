a7c70ffc5458f1d403dfe9943ecb715b382b41cd
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2022-01-18 23:07:16 +0530

    Create #3.c

---
+++ #3.c
@@ -0,0 +1,23 @@
+//3. Program to accept values of two numbers and print their addition.
+
+#include<stdio.h>
+
+void main()
+
+{
+
+  int a,b,ans;
+
+  printf("Enter 1st number:");
+
+  scanf("%d",&a);
+
+  printf("Enter 2nd number:");
+
+  scanf("%d",&b);
+
+  ans = a + b;
+
+  printf("Addition is : %d",ans);
+
+}
