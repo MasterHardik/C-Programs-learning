5280098956e99a4e61886f7eaaff6c8cf3f1ceb3
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2022-01-18 23:44:37 +0530

    Create #10.c

---
+++ #10.c
@@ -0,0 +1,25 @@
+//10. Program to check whether the number is even or odd.
+
+#include<stdio.h>
+
+void main()
+
+{
+
+  int n;
+
+  printf("Enter number: );
+
+  scanf("%d",&n);
+
+  if(n%2==0)
+
+    printf("Number is even");
+
+  else
+
+    printf("Number is odd");
+
+  
+
+}
